d556308d8a44b162e0afb4599d5233c9e8525e9b
PeshinaA <62890415+PeshinaA@users.noreply.github.com>
2020-04-10 16:54:21 +0300

    Add files via upload

---
+++ kp.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main(int argc, char* argv[]){
+
+	if(argc!=3){
+		cout<<"Error"<<endl;
+		return 1;
+	}
+
+	ifstream f(argv[1]);
+	ofstream f2(argv[2]);
+
+	if(f.is_open())
+		cout<<"OK"<<endl;
+	else{
+		cout<<"Error"<<endl;
+		return 1;
+	}
+
+	std::string s;
+	std::getline(f,s);
+	int a = s.find(' ');
+	int b = s.rfind(' ');
+	s.erase(b);
+	s.erase(0, a+1);
+
+	f2<<s;
+
+	f.close();
+
+	return 0;
+}
+
